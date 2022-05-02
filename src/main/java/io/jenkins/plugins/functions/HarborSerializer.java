ce342b69fcb28625250c5f77d2e17c8c980c252b
Gabriel cuadros <gabrielcuadros@gmail.com>
2022-05-02 12:33:06 -0500

    [JENKINS-0] update serializer to support verification of malformed url or uri

--- src/main/java/io/jenkins/plugins/functions/HarborSerializer.java
+++ src/main/java/io/jenkins/plugins/functions/HarborSerializer.java
@@ -3,11 +3,24 @@ package io.jenkins.plugins.functions;
 import hudson.model.Run;
 
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public abstract class HarborSerializer implements Serializable {
     private static final long serialVersionUID = 1L;
 
     public static void passValues(Run<?, ?> build, String URL){
-        HarborToAction.passValues(build,URL);
+        try {
+            URL url = new URL(URL);
+            URI uri =url.toURI();
+            HarborToAction.passValues(build,uri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        ;
     }
 }
